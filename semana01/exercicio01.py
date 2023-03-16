c180ce4afad85b5857df03e13f57cc19c7fe6397
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-03-16 19:24:22 -0300

    Update exercicio01.py

--- semana01/exercicio01.py
+++ semana01/exercicio01.py
@@ -1,1 +1,19 @@
-print ("hello")
+idade = "18 anos"
+nome = "Otávio Ferreira"
+cpf = "037.914.440-93"
+peso = "101 kg"
+dataDeNascimento = "03/06/2004"
+cidade = "Restinga Seca"
+estado = "Rio Grande do Sul"
+velocidade = "14 Km/h"
+preco = "R$ 3.500.000"
+
+print (idade)
+print (nome)
+print (cpf)
+print (peso)
+print (dataDeNascimento)
+print (cidade)
+print (estado)
+print (velocidade)
+print (preco)
