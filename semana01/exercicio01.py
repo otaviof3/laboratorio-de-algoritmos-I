5331d481c44bf63d0387b708bfd90a493d84cd29
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-03-16 20:55:58 -0300

    Update exercicio01.py

--- semana01/exercicio01.py
+++ semana01/exercicio01.py
@@ -1,19 +1,19 @@
-idade = "18 anos"
-nome = "Otávio Ferreira"
-cpf = "037.914.440-93"
-peso = "101.0 kg"
-dataDeNascimento = "03/06/2004"
-cidade = "Restinga Seca"
-estado = "Rio Grande do Sul"
-velocidade = "1400 Km/h"
-preco = "R$ 3500.00"
+idadeInt = "18"
+nomeChar = "Otávio Ferreira Dahlke"
+cpfChar = "037.914.440-93"
+pesoFloat = "101.00"
+dataDeNascimentoChar = "03/06/2004"
+cidadeChar = "Restinga Seca"
+estadoChar = "Rio Grande do Sul"
+velocidadeFloat = "1400.00"
+precoFloat = "3500.00"
 
-print (idade)
-print (nome)
-print (cpf)
-print (peso)
-print (dataDeNascimento)
-print (cidade)
-print (estado)
-print (velocidade)
-print (preco)
+print (idadeInt,"anos")
+print (nomeChar)
+print (cpfChar)
+print (pesoFloat,"kg")
+print (dataDeNascimentoChar)
+print (cidadeChar)
+print (estadoChar)
+print (velocidadeFloat,"Km/h")
+print ("R$",precoFloat)
