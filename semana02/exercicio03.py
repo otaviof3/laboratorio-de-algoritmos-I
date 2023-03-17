f655c45cd325dc68db39ed7b95b908e4ec7022d0
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-03-16 22:01:21 -0300

    Add files via upload

---
+++ semana02/exercicio03.py
@@ -0,0 +1,6 @@
+nome = "Otávio Ferreira Dahlke"
+idade = 18
+sexo = "Masculino"
+curso = "Sistemas de Informação"
+
+print("Olá, meu nome é",nome,",sou do sexo",sexo,",tenho",idade,"anos, e estudo",curso,".")
