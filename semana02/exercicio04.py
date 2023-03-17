9beb884020ea1135ca4a3d46e5265858f1b34585
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-03-16 22:08:05 -0300

    Add files via upload

---
+++ semana02/exercicio04.py
@@ -0,0 +1,7 @@
+precoFabrica = 35.00
+
+print("O preço de fábrica de uma peça de roupa é R$",precoFabrica)
+
+valorFinal = precoFabrica + (precoFabrica * 0.30)
+
+print("O seu valor final é R$",valorFinal)
