f272417b7b5264835bbb4f7578daf3ed85e9c68d
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-03-16 22:09:30 -0300

    Update exercicio02.py

--- semana02/exercicio02.py
+++ semana02/exercicio02.py
@@ -5,7 +5,7 @@ print ("A base do retângulo é",base)
 print ("A altura do retÂngulo é",altura)
 
 area = base * altura
-perimetro = base + base + altura + altura
+perimetro = (base * 2) + (altura * 2)
 
 print ("A área do retângulo é:",area)
 print ("O perímetro do retângulo é:",perimetro)
