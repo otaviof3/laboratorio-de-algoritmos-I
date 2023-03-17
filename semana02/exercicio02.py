4a58e38f6db4cb4af6fc47a29b4917f617763143
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-03-16 21:54:02 -0300

    Add files via upload

---
+++ semana02/exercicio02.py
@@ -0,0 +1,11 @@
+base = 7
+altura = 9
+
+print ("A base do retângulo é",base)
+print ("A altura do retÂngulo é",altura)
+
+area = base * altura
+perimetro = base + base + altura + altura
+
+print ("A área do retângulo é:",area)
+print ("O perímetro do retângulo é:",perimetro)
