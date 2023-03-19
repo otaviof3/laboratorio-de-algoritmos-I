fa0089ad84893730d0a532cd7bc8b1f55e99c012
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-03-19 18:13:19 -0300

    Update exercicio03.py

--- semana02/exercicio03.py
+++ semana02/exercicio03.py
@@ -3,4 +3,4 @@ idade = 18
 sexo = "Masculino"
 curso = "Sistemas de Informação"
 
-print("Olá, meu nome é",nome,",sou do sexo",sexo,",tenho",idade,"anos, e estudo",curso,".")
+print("Olá, meu nome é",nome,",sou do sexo",sexo,",tenho",idade,"anos e estudo",curso,".")
