36dcf6bf49cf86c0a99bbd56bf94378241bfe7d3
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-03-23 21:17:48 -0300

    Add files via upload

---
+++ semana03/exercicio03.py
@@ -0,0 +1,7 @@
+anoDeNascimento = int(input("Digite o seu ano de nascimento: "))
+idade = 2023 - anoDeNascimento
+if idade >= 16:
+    print ("Você pode votar!")
+else:
+    print ("Você não pode votar...")
+    
