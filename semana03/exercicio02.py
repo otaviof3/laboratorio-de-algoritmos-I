65c0a99fb01ac320f788abc46e21b6bb5895b0e4
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-03-23 21:12:07 -0300

    Add files via upload

---
+++ semana03/exercicio02.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+
+if n1 < n2:
+    print ("Ordem crescente:",n1,n2)
+else: 
+    print ("Ordem crescente:",n2,n1)
