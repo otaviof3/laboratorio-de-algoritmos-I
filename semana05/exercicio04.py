3ed2623f3b1a89c70583bbbc8399a814f2b8b4fa
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-04-09 16:18:24 -0300

    Update exercicio04.py

--- semana05/exercicio04.py
+++ semana05/exercicio04.py
@@ -2,7 +2,7 @@ idade = 0
 peso = 0
 pessoas = 0
 maiorDeI = 0
-entreDezTrinta = 0
+entre1030 = 0
 while pessoas < 7:
     idadeP = int(input("Digite sua idade: "))
     pesoP = float(input("Digite seu peso: "))
@@ -10,10 +10,11 @@ while pessoas < 7:
     pessoas = pessoas + 1
     if idadeP >= 18:
         maiorDeI = maiorDeI + 1
-    if idade >= 10 and idade <= 30:
-        entreDezTrinta = entreDezTrinta + 1
+    if idadeP >= 10 and idadeP <= 30:
+        entre1030 = entre1030 + 1
 mediaI = idade / pessoas
-porcentagemDezTrinta = entreDezTrinta / 100
+q1030 = entre1030 / 7
+porcentagem1030 = q1030 * 100
 print ("A média das idades é",mediaI)
 print ("A quantidade de pessoas maiores de idade é",maiorDeI)
-print ("A porcentagem de pessoas entre dez e trinta anos é",porcentagemDezTrinta)
+print ("A porcentagem de pessoas entre dez e trinta anos é",porcentagem1030)
