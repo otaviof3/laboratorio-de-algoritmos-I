6286a7f16a3324edac0c9c3dfa9f558a5ac59279
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-04-13 15:04:05 -0300

    Update exercicio06.py

--- semana05/exercicio06.py
+++ semana05/exercicio06.py
@@ -1,9 +1,9 @@
 idade = 0
 pessoas = 0
-maiorQue18 = 0
+maiorIgual18 = 0
 while pessoas < 10:
     idade = int(input("Digite sua idade: "))
     if idade >= 18:
-        maiorQue18 = maiorQue18 + 1
+        maiorIgual18 = maiorIgual18 + 1
     pessoas = pessoas + 1
-print("A quantidade de pessoas com idade maior ou igual a 18 é",maiorQue18)
+print("A quantidade de pessoas com idade maior ou igual a 18 é",maiorIgual18)
