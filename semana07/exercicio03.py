20a72716720e0e2242ad1b50c42e740ffca3be3d
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-04-20 20:26:51 -0300

    Add files via upload

---
+++ semana07/exercicio03.py
@@ -0,0 +1,11 @@
+numero1 = 1
+numero2 = 1
+contador = 3
+print(numero1)
+print(numero2)
+for contador in range (1,9):
+    contador = contador + 1
+    numero = numero1 + numero2
+    print (numero)
+    numero1 = numero2
+    numero2 = numero
